b4c68eda5f6570f58f64ca4bd90fa0fa57f40a68
Alexander Buese <alexander.buese@gmx.de>
2012-12-29 15:37:24 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoblogLab::Application.config.secret_token = 'd224d740fbf3970ece1e49bea517a5d7a91d135194159c7e7c49be08fec326b45a50e10d424d626d04937a31b65cdd6d30d91f53b5b0c3cd9d8add5095a1f0e1'
