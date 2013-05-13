1680702054a6b3e11a6c71037b2d49429ad66653
Macropod <user@mail.org>
2013-05-13 18:03:48 +1000

    Reverse order of gallery list

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,4 +2,5 @@ class Gallery < ActiveRecord::Base
   attr_accessible :name
   has_many :posts, dependent: :destroy
   validates(:name, presence: true, length: { maximum: 100 }, uniqueness: { case_sensitive: true })
+  default_scope order: 'galleries.created_at DESC'
 end
